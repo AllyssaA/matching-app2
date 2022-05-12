436a3ca02ed84d481fb8605796cf8149f0d0c763
allyssaa <a.alimoestar@gmail.com>
2022-05-12 12:34:48 +0200

    :construction::sparkles: templating /pages router

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
+
+app.use(express.static('static'))
 
 app.set('views', 'views')
 
@@ -10,10 +12,24 @@ app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
     res.render('index', {
-        pageTitle: 'Matching app'
+        pageTitle: 'Matching booklist app',
+        title: 'Profile'
     })
 })
 
+app.get('/add', (req, res) => {
+    res.render('addBook', {
+        pageTitle: 'Add a book',
+        title: 'Add a book to list'
+    })
+})
+
+app.get('/list', (req, res) => {
+    res.render('bookList', {
+        pageTitle: 'Book list',
+        title: 'Booklist'
+    })
+})
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
